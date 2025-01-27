d2f9fb4729e518c88946b5a5357dc399967e7219
Cardano9-tech <wikas123987@gmail.com>
2025-01-26 20:19:42 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth Scroll for Navigation Links
-document.querySelectorAll('.navbar a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
